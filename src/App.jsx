093f7b23904918f9740394422d3c071334705176
Quasark <q@quasark.xyz>
2023-11-06 12:31:34 -0800

    lab 36 init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <header className="App-header">
+      <h1>This is a new app</h1>
+    </header>
+    </>
+  )
+}
+
+export default App
